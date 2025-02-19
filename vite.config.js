16fd1d6ff7b4aaa18360e3577f11491d39b05282
deepan27 <deepannandirahian@alphareds.com>
2025-02-19 23:06:07 +0800

    Add GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Deepan27/cleaning-service-website',
+  base: '/cleaning-service/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
